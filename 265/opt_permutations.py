8f10bf878895e546b91bd8d9a07b8d2efcf2df27
Niklas Jonsson <niklasandersjonsson@gmail.com>
2016-05-31 22:16:09 +0200

    Done with permutation optimization

--- 265/opt_permutations.py
+++ 265/opt_permutations.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import re
+import math
 
 def parse(sentence):
    return [int(item) for item in re.findall(r'\d+', sentence)]
@@ -24,9 +25,36 @@ def i_perm_of_n(i, n):
         perm.append(num_list.pop(digit))
     return perm
 
+def decimal_of(fac_list):
+    num = 0
+    for i in range(1,len(fac_list)):
+        num = num + math.factorial(i)*fac_list[-i-1]
+    return num
+        
+    
+def find_idx(elem, l):
+    if l == []:
+        l.append(elem)
+        return 0
+    i = 0
+    while i < len(l) and elem > l[i]:
+        i += 1
+    l.insert(i, elem)
+    return i
+
+def perm_idx_of(perm):
+    l = []
+    fac_list = []
+    perm = [int(x) for x in perm]
+    for p in perm[::-1]:
+        idx = find_idx(p,l)
+        fac_list.append(idx)
+    return decimal_of(fac_list[::-1])
+'''
 while (True):
     cmd = raw_input()
     if cmd == "q":
         break
     nums = parse(cmd)
     print(i_perm_of_n(nums[0], nums[1]))
+'''
