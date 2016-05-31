b3b93f3b8411f7cffe3acc41e91a868b1c87ce5c
Niklas Jonsson <niklasandersjonsson@gmail.com>
2016-05-31 18:25:03 +0200

    Done with challenge 265

--- 265/combinations.py
+++ 265/combinations.py
@@ -2,20 +2,25 @@
 import re
 
 def parse(sentence):
-   return [int(item) for item in re.findall(r'\d+', sentence)]
+    return [int(item) for item in re.findall(r'\d+', sentence)]
 
-def comb(k, n):
-    for i in range(n):
-        for j in range(
+def comb(k, nlist):
+    if k == 0:
+        return [[]]
+    ret = []
+    for i in range(len(nlist)):
+        combs = comb(k-1, nlist[i+1:])
+        for c in combs:
+            c.insert(0,nlist[i])
+            ret.append(c)
+    return ret
 
-
-def (i, k, n):
-    return comb
+def gen_comb(i, k, n):
+    return comb(k,range(n))[i-1]
 
 while (True):
     cmd = raw_input()
     if cmd == "q":
         break
     nums = parse(cmd)
-    print(i_perm_of_n(nums[0], nums[1]))
-
+    print(gen_comb(nums[0], nums[1], nums[2]))
