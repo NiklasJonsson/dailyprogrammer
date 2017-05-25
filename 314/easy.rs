41795d037f6d41402ee309896f7836b75d382c8d
Niklas Jonsson <niklasandersjonsson@gmail.com>
2017-05-25 16:50:55 +0200

    Fixed bug and tests now read from file

--- 314/easy.rs
+++ 314/easy.rs
@@ -1,12 +1,41 @@
-use std::io; 
+use std::io::BufReader;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn cmp_str_lrg(a : &str, b : &str) -> std::cmp::Ordering{
+    let mut ab = String::from(a);
+    ab.push_str(b);
+    let mut ba = String::from(b);
+    ba.push_str(a);
+    let x:i32 = ab.parse().unwrap();
+    let y:i32 = ba.parse().unwrap();
+    return y.cmp(&x);
+}
+
+fn cmp_str_sml(a : &str, b : &str) -> std::cmp::Ordering{
+    let mut ab = String::from(a);
+    ab.push_str(b);
+    let mut ba = String::from(b);
+    ba.push_str(a);
+    let x:i32 = ab.parse().unwrap();
+    let y:i32 = ba.parse().unwrap();
+    return x.cmp(&y);
+}
 
 fn main() {
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).expect("Failed stdin");
-    let mut numbers: Vec<&str> = line.split_whitespace().collect();
-    numbers.sort_by(|a, b| b.cmp(a));
-    let largest = numbers.join("");
-    numbers.sort();
-    let smallest = numbers.join("");
-    println!("{} {}", smallest, largest);
+    let in_path = env::args().nth(1).unwrap();
+
+    let file = File::open(&in_path).unwrap();
+    let bfr = BufReader::new(file);
+
+    for line in bfr.lines() {
+        let l = line.unwrap();
+        let mut numbers: Vec<&str> = l.split_whitespace().collect();
+        numbers.sort_by(|a, b| cmp_str_lrg(a,b));
+        let largest = numbers.join("");
+        numbers.sort_by(|a, b| cmp_str_sml(a,b));
+        let smallest = numbers.join("");
+        println!("{} {}", smallest, largest);
+    }
 }
--- 314/inter.rs
+++ 314/inter.rs
@@ -1,24 +1,33 @@
-use std::io; 
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 fn rotate(a: &String, pos : usize) -> String {
     return format!("{}{}", a.chars().skip(pos).collect::<String>(), a.chars().take(pos).collect::<String>());
 }
 
 fn main() {
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).expect("Failed stdin");
-    let word = String::from(line.trim());
-    let mut pos = 0;
-    let mut min = word.clone(); 
-    for i in 0..word.len() {
-        let t = rotate(&word, i);
-        if t < min {
-            min = t;
-            pos = i;
+    let in_path = env::args().nth(1).unwrap();
+    let file = File::open(&in_path).unwrap();
+    let bfr = BufReader::new(file);
+
+    for l in bfr.lines() {
+        let line = l.unwrap();
+        let word = String::from(line.trim());
+        let mut pos = 0;
+        let mut min = word.clone(); 
+        for i in 0..word.len() {
+            let t = rotate(&word, i);
+            if t < min {
+                min = t;
+                pos = i;
+            }
         }
+
+        let start : String = word.chars().skip(pos).collect();
+        let end : String = word.chars().take(pos).collect();
+        println!("{} {}{}",pos, start, end);
     }
 
-    let start : String = word.chars().skip(pos).collect();
-    let end : String = word.chars().take(pos).collect();
-    println!("{} {}{}",pos, start, end);
 }
